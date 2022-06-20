a5c1b0d3747b3726355851ecad94ebb8f8554bac
ShozoTanaka <shozotanaka.japan@gmail.com>
2022-06-21 08:44:12 +0900

    プロジェクト ファイルを追加します。

---
+++ Direct3D Framework/GraphScene.cpp
@@ -0,0 +1,237 @@
+#include "pch.h"
+#include <memory>
+#include <utility>
+#include <commdlg.h>
+#include <iostream>
+#include <fstream>
+#include "GraphScene.h"
+#include "Keyboard.h"
+#include "Mouse.h"
+#include "Model.h"
+#include "Game.h"
+
+// コンストラクタ
+GraphScene::GraphScene(Game* game)
+	:
+	m_game(game),									// Gameクラス
+	m_directXGraphics(nullptr),				// DirectXGraphicsクラス
+	m_device(nullptr),								// Deviceクラス
+	m_context(nullptr),								// DeviceContextクラス
+	m_keyboardState{},								// キーボードステート
+	m_mouseCursorPosition{},					// マウスカーソル位置
+	m_mouseState{},									// マウスステート
+	m_mouseStateTracker{},						// マウスステートトラッカー
+	m_worldMatrix{},									// ワールド
+	m_viewMatrix{},									// ビュー
+	m_projectionMatrix{},							// プロジェクション
+	m_cameraRotation{},							// カメラ回転
+	m_cameraPosition{},							// カメラ位置
+	m_cameraFocus{},								// カメラフォーカス
+	m_rotaionAngle(0.0f),							// 角度
+	m_distance(10.0f),								// 注視点から視点までの距離
+	m_zoom(1.0f),										// ズーム
+	m_fov(DirectX::XM_PI / 4.0f),				// フィールドオブビュー
+	m_nearPlane(0.1f),								// ニアクリップ
+	m_farPlane(0.0f),								// ファークリップ
+	m_scale(1.0f)										// スケール
+{
+	// DirectX Graphicsクラスのインスタンスを取得する
+	m_directXGraphics = DirectXGraphics::GetInstance();
+	// デバイスを取得する
+	m_device = DirectXGraphics::GetInstance()->GetDeviceResources()->GetD3DDevice();
+	// デバイスコンテキストを取得する
+	m_context = DirectXGraphics::GetInstance()->GetDeviceResources()->GetD3DDeviceContext();
+}
+
+// デストラクタ
+GraphScene::~GraphScene()
+{
+
+}
+
+// 初期化する
+void GraphScene::Initialize()
+{
+
+}
+
+// 更新する
+void GraphScene::Update(const DX::StepTimer& timer)
+{
+	// キーボードの状態を取得する
+	m_keyboardState = m_game->GetKeyboard()->GetState();
+	// キーボードの状態を更新する
+	m_game->GetKeyboardTracker().Update(m_keyboardState);
+
+	// マウスの状態を取得する
+	m_mouseState = m_game->GetMouse()->GetState();
+	// マウストラッカーを更新する
+	m_mouseStateTracker.Update(m_mouseState);
+
+	// 視点ベクトルを取得する
+	auto eyePosition = m_game->GetCamera()->GetEyePosition();
+	// 視点と注視点の距離を計算する
+	m_distance = eyePosition.Length();
+
+	// 平面を初期化する
+	DirectX::SimpleMath::Plane plane(0.0f, 1.0f, 0.0f, 0.0f);
+	// マウスカーソルのスクリーン位置を取得する
+	m_mouseCursorPosition = DirectX::SimpleMath::Vector2(roundf((float)m_mouseState.x), roundf((float)m_mouseState.y));
+	// カメラをコントロールする
+	ControlCamera(timer);
+}
+
+// アークボールを使用してカメラをコントロールする
+void GraphScene::ControlCamera(const DX::StepTimer& timer)
+{
+	// 経過時間を取得する
+	float elapsedTime = float(timer.GetElapsedSeconds());
+	// スケールゲイン
+	constexpr float SCALE_GAIN = 0.001f;
+
+	// カメラ移動行列
+	DirectX::SimpleMath::Matrix im;
+	m_viewMatrix.Invert(im);
+	DirectX::SimpleMath::Vector3 move = DirectX::SimpleMath::Vector3::TransformNormal(move, im);
+
+	// マウスの移動が相対マウス移動である場合
+	if (m_game->GetMouse()->GetState().positionMode == DirectX::Mouse::MODE_RELATIVE)
+	{
+		DirectX::SimpleMath::Vector3 delta = DirectX::SimpleMath::Vector3(-float(m_mouseState.x), float(m_mouseState.y), 0.f) * m_distance;
+		delta = DirectX::SimpleMath::Vector3::TransformNormal(delta, im);
+		// カメラフォーカス位置を計算する
+		m_cameraFocus += delta * elapsedTime;
+	}
+	// マウスの右ボタンをドラッグしている場合
+	else if (m_ballCamera.IsDragging())
+	{
+		// マウスの移動
+		m_ballCamera.OnMove(m_mouseState.x, m_mouseState.y);
+		// ボールカメラの現在のクォータニオンを取得する
+		auto q = m_ballCamera.GetQuaternion();
+		// カメラ回転の逆コォータニオンを計算する
+		q.Inverse(m_cameraRotation);
+	}
+	else
+	{
+		// マウスフォイールを回転させた場合のズーム値を計算する
+		m_zoom = 1.0f + float(m_mouseState.scrollWheelValue) * SCALE_GAIN;
+		// ズーム値を調整する
+		m_zoom = std::max(m_zoom, 0.01f);
+		// スクロールフォイール値をリセットする
+		m_game->GetMouse()->ResetScrollWheelValue();
+	}
+
+	// ドラッグ中でない場合
+	if (!m_ballCamera.IsDragging())
+	{
+		// マウスの右ボタンを押し下げている場合
+		if (m_mouseStateTracker.rightButton == DirectX::Mouse::ButtonStateTracker::PRESSED)
+		{
+			// 左右の[Ctrlキー]を押し下げている場合
+			if (m_keyboardState.LeftControl || m_keyboardState.RightControl)
+			{
+				// ボールカメラを開始する
+				m_ballCamera.OnBegin(m_mouseState.x, m_mouseState.y);
+			}
+		}
+	}
+	// マウスの右ボタンを解放している場合
+	else if (m_mouseStateTracker.rightButton == DirectX::Mouse::ButtonStateTracker::RELEASED)
+	{
+		// ボールカメラを終了する
+		m_ballCamera.OnEnd();
+	}
+	// カメラの向きを更新する
+	auto direction = DirectX::SimpleMath::Vector3::Transform(DirectX::SimpleMath::Vector3::Backward, m_cameraRotation);
+	// カメラ位置を計算する
+	m_cameraPosition = m_cameraFocus + (m_distance * m_zoom) * direction;
+	// 視線ベクトルを設定する
+	m_game->GetCamera()->SetEyePosition(m_cameraPosition);
+}
+
+// 描画する
+void GraphScene::Render()
+{
+	const DirectX::XMVECTORF32 xaxis = { 100.f, 0.0f, 0.0f };
+	const DirectX::XMVECTORF32 yaxis = { 0.0f, 0.0f, 100.f };
+
+	// グリッドを描画する
+	DrawGrid(xaxis, yaxis, DirectX::g_XMZero, 10, 10, DirectX::Colors::DarkGray);
+}
+
+// 後処理を行う
+void GraphScene::Finalize()
+{
+
+}
+
+
+// グリッドを描画する
+void GraphScene::DrawGrid(
+	DirectX::FXMVECTOR xAxis,
+	DirectX::FXMVECTOR yAxis,
+	DirectX::FXMVECTOR origin,
+	size_t xdivs,
+	size_t ydivs,
+	DirectX::GXMVECTOR m_color
+)
+{
+	// パフォーマンス開始イベント
+	m_directXGraphics->GetDeviceResources()->PIXBeginEvent(L"Draw Grid");
+	// プリミティブ描画を開始する
+	m_directXGraphics->DrawPrimitiveBegin(m_directXGraphics->GetViewMatrix(), m_directXGraphics->GetProjectionMatrix());
+
+	xdivs = std::max<size_t>(1, xdivs);
+	ydivs = std::max<size_t>(1, ydivs);
+
+	for (size_t index = 0; index <= xdivs; ++index)
+	{
+		float percent = float(index) / float(xdivs);
+		percent = (percent * 2.0f) - 1.0f;
+		// スケールを計算する
+		DirectX::XMVECTOR scale = DirectX::XMVectorScale(xAxis, percent);
+		scale = DirectX::XMVectorAdd(scale, origin);
+		// 頂点1を設定する
+		DirectX::VertexPositionColor v1(DirectX::XMVectorSubtract(scale, yAxis), m_color);
+		// 頂点2を設定する
+		DirectX::VertexPositionColor v2(DirectX::XMVectorAdd(scale, yAxis), m_color);
+		// 直線を描画する
+		m_directXGraphics->GetPrimitiveBatch()->DrawLine(v1, v2);
+	}
+
+	for (size_t index = 0; index <= ydivs; index++)
+	{
+		float percent = float(index) / float(ydivs);
+		percent = (percent * 2.0f) - 1.0f;
+		// スケールを計算する
+		DirectX::XMVECTOR scale = DirectX::XMVectorScale(yAxis, percent);
+		scale = DirectX::XMVectorAdd(scale, origin);
+		// 頂点1を設定する
+		DirectX::VertexPositionColor v1(DirectX::XMVectorSubtract(scale, xAxis), m_color);
+		// 頂点2を設定する
+		DirectX::VertexPositionColor v2(DirectX::XMVectorAdd(scale, xAxis), m_color);
+		// 直線を描画する
+		m_directXGraphics->GetPrimitiveBatch()->DrawLine(v1, v2);
+	}
+	// プリミティブバッチを終了する
+	m_directXGraphics->DrawPrimitiveEnd();
+	// パフォーマンス終了イベント
+	m_directXGraphics->GetDeviceResources()->PIXEndEvent();
+}
+
+// プロジェクションを生成する
+void GraphScene::CreateProjection()
+{
+	// ウィンドウサイズを取得する
+	auto size = m_directXGraphics->GetDeviceResources()->GetOutputSize();
+	// プロジェクションを生成する
+	m_projectionMatrix = DirectX::SimpleMath::Matrix::CreatePerspectiveFieldOfView(m_fov, float(size.right) / float(size.bottom), m_nearPlane, m_farPlane);
+}
+
+// アークボールのためのウィンドウサイズを設定する
+void GraphScene::SetWindow(const int& width, const int& height)
+{
+	// アークボールのウィンドウサイズを設定する
+	m_ballCamera.SetWindow(width, height);
+}
